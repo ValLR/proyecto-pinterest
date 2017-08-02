e5b5233121e9ef2b8f98cc9d1154f3cb4a9ca1ca
ValLR <valentinalopezrecabal@gmail.com>
2017-08-02 15:53:38 -0400

    imprimiendo primeros 20 elementos de json, y dando estilos a cada div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-	res.forEach(function(el){
+	res.slice(-20).forEach(function(el){
 		console.log(el.image_url)
 		$("#json-content").append(`
-		     <div class="pinWrapper">
-		        <div class="pinImage">
-		          <img src="dist/img/`+ el.image_url +`" alt="">
-		        </div>
-		     </div>
+		    <div class="elemento">
+		        <img class="pin-image" src="dist/img/`+ el.image_url +`" alt="">
+		        <h3 class="pin-title">`+ el.title +`</h3>
+		        <p class="pin-description">`+ el.description +`</p>
+		        <p class="pin-credit"><a href="#!" id="name">`+ el.username +`</a> desde Pinterest <a href="#!" id="hashtag">#`+ el.hashtag +`</a></p>
+		    </div>
 			`)
 	})
 });
