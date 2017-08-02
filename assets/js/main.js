59d677e8b7b529be64a3325daba043d7ae1a6ac6
ValLR <valentinalopezrecabal@gmail.com>
2017-08-02 03:14:52 -0400

    imprimiendo fotos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
-})
+	res.forEach(function(el){
+		console.log(el.image_url)
+		$("#json-content").append(`
+		     <div class="pinWrapper">
+		        <div class="pinImage">
+		          <img src="dist/img/`+ el.image_url +`" alt="">
+		        </div>
+		     </div>
+			`)
+	})
+});
