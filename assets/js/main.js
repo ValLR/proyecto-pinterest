41486490699b4f1ad35468a4d4fff25551422cbb
ValLR <valentinalopezrecabal@gmail.com>
2017-08-03 11:31:51 -0400

    cambiando clases porque no usé style.scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 		<a id="`+ el.id +`" class= "modalC" ><div class="pinterest__contenedor">
 			<div class="pinterest__box">
 				<img id="`+ el.id +`" src="dist/img/`+ el.id +`.jpg">
-				<h3 class="pinterest__box--title">`+ el.title +`</h3>
-				<h5 class="pinterest__box--description">`+ el.description +`</h5>
-				<p><a class="pinterest__box--username">`+ el.username +`</a> desde Pinterest <a href="#!" class= "pinterest__box--hashtag">#`+ el.hashtag +`</a></p>
+				<h3 class="title">`+ el.title +`</h3>
+				<h5 class="description">`+ el.description +`</h5>
+				<p><a class="username">`+ el.username +`</a> desde Pinterest <a href="#!" class= "pinterest__box--hashtag">#`+ el.hashtag +`</a></p>
 				<hr>
 			</div>
 		</div></a>
