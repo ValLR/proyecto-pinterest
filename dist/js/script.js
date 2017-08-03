848875e4e3acd2ed5a63b2e8620d75283860108d
ValLR <valentinalopezrecabal@gmail.com>
2017-08-03 07:17:49 -0400

    modal no imprime

--- dist/js/script.js
+++ dist/js/script.js
@@ -10428,7 +10428,7 @@ $(document).ready(function(){
 		var pintContent =`
 		<a class= "modalC" ><div class="pinterest__contenedor">
 			<div class="pinterest__box">
-				<img src="dist/img/`+ el.id +`.jpg">
+				<img id="`+ el.id +`" src="dist/img/`+ el.id +`.jpg">
 				<h3 class="pinterest__box--title">`+ el.title +`</h3>
 				<h5 class="pinterest__box--description">`+ el.description +`</h5>
 				<p><a class="pinterest__box--username">`+ el.username +`</a> desde Pinterest <a href="#!" class= "pinterest__box--hashtag">#`+ el.hashtag +`</a></p>
@@ -10441,6 +10441,8 @@ $(document).ready(function(){
 		$(".pinterest").append(pintContent);
 
 		$(".modalC").click(function(){
+			var id = $(".modalC").attr("id");
+			$("#imgMod-container").append(`<img id="`+ el.id +`" src="dist/img/`+ el.id +`.jpg">`)
 			modal.style.display="block"
 		});
 		$(".close").click(function(){
