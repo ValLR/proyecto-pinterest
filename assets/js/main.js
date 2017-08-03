7cdd1f8c76245078bf9ee364e69d32a52bab47ed
ValLR <valentinalopezrecabal@gmail.com>
2017-08-03 05:52:24 -0400

    hecho el modal, sin nada que imprimir aún

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,26 @@ $(document).ready(function(){
 	res.forEach(function(el){
 		console.log(el.image_url);
 		var pintContent =`
-		<a href="!#" class= "modalC" ><div class="pinterest__contenedor">
+		<a class= "modalC" ><div class="pinterest__contenedor">
 			<div class="pinterest__box">
 				<img src="dist/img/`+ el.id +`.jpg">
 				<h3 class="pinterest__box--title">`+ el.title +`</h3>
 				<h5 class="pinterest__box--description">`+ el.description +`</h5>
 				<p><a class="pinterest__box--username">`+ el.username +`</a> desde Pinterest <a href="#!" class= "pinterest__box--hashtag">#`+ el.hashtag +`</a></p>
+				<hr>
 			</div>
 		</div></a>
 
 		`
 		pintContent= $(pintContent);
 		$(".pinterest").append(pintContent);
+
+		$(".modalC").click(function(){
+			modal.style.display="block"
+		});
+		$(".close").click(function(){
+			modal.style.display="none"
+		})
+
 	});
 });
